3ae0fe4dcdec08c77d4030d546f97d3e9738ee98
John Boyle <nikolasboyle@gmail.com>
2020-10-01 12:31:01 -0700

    Add step model

--- index.js
+++ index.js
@@ -15,8 +15,28 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/apple-health", async (req, res) => {
-  console.log(req.body);
-  res.status(200).end();
+  try {
+    const { data } = req.body;
+
+    for (let i = 0; i < data.length; i++) {
+      const element = data[i];
+
+      // Add more activity types in the future
+      switch (element.type) {
+        case "Steps":
+          await Step.create(element);
+          break;
+        default:
+          break;
+      }
+    }
+
+    console.log(data);
+    res.status(200).end();
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error });
+  }
 });
 
 app.listen(port, () => {
